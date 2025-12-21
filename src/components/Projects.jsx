a799fbf7ef5e26034d59119c5f6c83d470386d58
Sunny Mudgal <sunnyxmudgal@gmail.com>
2025-12-22 01:16:34 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -39,11 +39,7 @@ function Projects() {
                 className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
               />
 
-              <div className="absolute inset-0 transition flex items-center justify-center">
-                <span className="text-white text-lg font-semibold">
-                  BlueBird E-Commerce
-                </span>
-              </div>
+            
             </div>
 
             {/* Content */}
