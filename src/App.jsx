924b200cbde8ec175743ed5f870f93c41e0b5c4c
Sunny Mudgal <sunnyxmudgal@gmail.com>
2025-12-22 02:02:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from "./components/Projects";
 import LocomotiveScroll from "locomotive-scroll";
 import "./index.css";
 import Footer from "./components/Footer";
+import { Navbar } from "./Navbar";
 
 function App() {
   useEffect(() => {
@@ -18,7 +19,11 @@ function App() {
 
   return (
   
-    <div className="w-full min-h-screen bg-zinc-900 text-white">
+    <div className="w-full min-h-screen bg-zinc-900 text-white relative">
+      
+  <div className="absolute top-5 flex items-center justify-center">
+     <Navbar/> 
+  </div>
       <section id="home">
         <LandingPage />
         <Marquee/>
